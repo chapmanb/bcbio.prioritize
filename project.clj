b1630f5b320c7871b2b670249ac8a5e014839d2a
chapmanb <chapmanb@50mail.com>
2019-01-02 07:09:19 -0500

    Update dependencies to avoid warning messages
    Synchronize with latest upstream fixes in bcbio.run and recall

--- project.clj
+++ project.clj
@@ -7,15 +7,13 @@
                  [org.clojure/data.csv "0.1.2"]
                  [semantic-csv "0.1.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [clj-biosequence "0.2.6"]
                  [clj-http "3.9.1"]
                  [cheshire "5.8.1"]
                  [clj-time "0.15.1"]
                  [slingshot "0.12.2"]
-                 [com.taoensso/timbre "3.4.0"]
                  [gavagai "0.3.2"]
-                 [bcbio.variation.recall "0.1.4"]
+                 [bcbio.variation.recall "0.2.0"]
                  [com.github.samtools/htsjdk "1.140"]
-                 [bcbio.run "0.0.5"]]
+                 [bcbio.run "0.0.6"]]
   :profiles {:uberjar {:aot [bcbio.prioritize.main]}}
   :main ^:skip-aot bcbio.prioritize.main)
